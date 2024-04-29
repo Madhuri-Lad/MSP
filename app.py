6b0e61746c38d4867df61080c7295173e33b8fe8
Madhuri-Lad <66513068+Madhuri-Lad@users.noreply.github.com>
2024-04-29 13:04:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app_name='Stock marketing analysis'
 st.title(app_name)
 st.subheader('This App is created to forcast the stock market price of the selected company')
 # add an image
-st.image('https://m.economictimes.com/thumb/msid-102245149,width-850,height-600,resizemode-4,imgsize-104000/unleashing-the-bulls-how-the-stock-market-achieved-unprecedented-record-levels.jpg')
+st.image('https://m.economictimes.com/thumb/msid-102245149,width-600,height-600,resizemode-4,imgsize-104000/unleashing-the-bulls-how-the-stock-market-achieved-unprecedented-record-levels.jpg')
 
 # take input from user
 st.sidebar.header('Select the parametes from below')
